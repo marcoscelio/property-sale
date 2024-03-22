3e32d5188a1b86e714bb452de29c18b02ed015f0
Marcos Celio <marcospaivacelio@gmail.com>
2024-03-22 14:14:52 -0300

    feat: config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/property-sale/",
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {

a06de10490796c01ea257786c89acd20981ac88d
Marcos Celio <marcospaivacelio@gmail.com>
2024-04-01 20:43:31 -0300

    fix: test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/property-sale/" : "/",
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://s3.us-west-2.amazonaws.com",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       // ws: true,
-  //       // rewrite: (path) => path.replace(/^\/api/, ""),
-  //       rewrite: (path) =>
-  //         path.replace(/^\/api/, "cdn.number8.com/LA/listings.json"),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://s3.us-west-2.amazonaws.com",
+        changeOrigin: true,
+        secure: false,
+        // ws: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) =>
+          path.replace(/^\/api/, "cdn.number8.com/LA/listings.json"),
+      },
+    },
+  },
 });
